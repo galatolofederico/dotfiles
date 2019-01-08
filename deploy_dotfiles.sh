c47578e5b9a083e494aba84d0441af05bba1534e
Federico Galatolo <galatolo.federico@gmail.com>
2019-01-08 19:32:42 +0100

    fix deploy bug

--- deploy_dotfiles.sh
+++ deploy_dotfiles.sh
@@ -1,4 +1,4 @@
-for file in $(find . -type f -not -path "./.git/*" -not -path "./README.md" -not -path "./LICENSE" ); do
+for file in $(find . -type f -not -path "./.git/*" -not -path "./README.md" -not -path "./LICENSE" -no -path "deploy_dotfiles.sh" -no -path "backup_old_dotfiles.sh" ); do
     abs_file=$(echo  $file | cut -c3-)
     dest_dir=$(dirname ~/$abs_file)
     mkdir -p -- $dest_dir
