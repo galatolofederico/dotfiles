3f121e086a4419f5feeddc796df1f5d7c0a7aa8c
Federico Galatolo <galatolo.federico@gmail.com>
2019-06-20 12:57:09 +0200

    Added templates

---
+++ .config/templates/mnist.py
@@ -0,0 +1,11 @@
+train_loader = torch.utils.data.DataLoader(
+    datasets.MNIST(
+	"/tmp/mnist_data", train=True, download=True, transform=transforms.Compose([
+	    transforms.ToTensor(),
+	])),batch_size=batch_size, shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(
+    datasets.MNIST(
+	"/tmp/mnist_data", train=False, download=True, transform=transforms.Compose([
+	    transforms.ToTensor()
+	])),batch_size=batch_size, shuffle=True)
