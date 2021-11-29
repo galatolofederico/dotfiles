d81aa80e623349aa55ef9c825138cd11b53642a2
Federico Galatolo <galatolo.federico@gmail.com>
2021-11-29 14:58:15 +0100

    better jack scripts

--- bin/start_jack_pulse.sh
+++ bin/start_jack_pulse.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
-jackd -dalsa -r192000 -p512 -D -Chw:Microphone -Phw:PCH &
-sleep 3;
+jackd -dalsa -r48000 -p256 -D -Chw:Microphone -Phw:PCH
+sleep 5;
+notify-send "Jack server started" -t 3000
+
+pactl unload-module module-jack-sink
+pactl unload-module module-jack-source
 
 pactl load-module module-jack-sink
 pactl load-module module-jack-source
 
 pacmd set-default-sink jack_out
 pacmd set-default-source jack_in
+
+
