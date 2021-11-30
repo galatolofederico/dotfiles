5ba28c4fe84d70f55164773c425ab5b7abd54602
Federico Galatolo <galatolo.federico@gmail.com>
2021-11-30 16:57:50 +0100

    more jack stuff

--- bin/start_jack_pulse.sh
+++ bin/start_jack_pulse.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-jackd -dalsa -r48000 -p256 -D -Chw:Microphone -Phw:PCH
+jackd -dalsa -r48000 -p256 -D -Chw:Microphone -Phw:PCH &
 sleep 5;
 notify-send "Jack server started" -t 3000
 
@@ -13,4 +13,9 @@ pactl load-module module-jack-source
 pacmd set-default-sink jack_out
 pacmd set-default-source jack_in
 
+sleep 2;
 
+carla -n $HOME/.config/jack-config.carxp &
+sleep 1;
+jack_disconnect system:capture_1 "PulseAudio JACK Source:mono"
+notify-send "Patchbay loaded" -t 3000
